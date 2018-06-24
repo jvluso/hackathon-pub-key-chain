03e7fdaecae229a858611e5bcaad11835a99a3df
Lou Wynn <lamdapro@gmail.com>
2018-06-24 12:33:54 -0700

    Add web3 header.

--- pkc/background.js
+++ pkc/background.js
@@ -1,3 +1,6 @@
+import Web3 from 'web3';
+const web3 = new Web3(window.web3.currentProvider);
+
 chrome.runtime.onInstalled.addListener(function () {
   chrome.identity.getAuthToken({ interactive: true }, authorizationCallback);
   chrome.identity.getProfileUserInfo(userInfo => {
