3f7fb5bf33d72d53885869784861e8f289f5756a
yidongw <wy.dong96@gmail.com>
2018-06-24 14:00:35 -0700

    web3

---
+++ web3.js
@@ -0,0 +1,9 @@
+import Web3 from 'web3';
+
+const web3 = new Web3(window.web3.currentProvider);
+
+const address = '0x8bf5986f5a2388ac9617f10333c8720c11760c32';
+
+//const abi =
+
+export default new web3.eth.Contract(abi, address);
